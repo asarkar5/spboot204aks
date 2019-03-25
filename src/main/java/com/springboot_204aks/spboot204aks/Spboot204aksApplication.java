0466bdc2c84bbd5e85c93d0e8d1426c2bf5cbc38
asarkar <asarkar@promnetwork.com>
2019-03-24 22:17:39 -0400

    Initial commit

---
+++ src/main/java/com/springboot_204aks/spboot204aks/Spboot204aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_204aks.spboot204aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spboot204aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spboot204aksApplication.class, args);
+	}
+
+}
